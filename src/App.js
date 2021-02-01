6eeebe3d3a9cea1e5372d38dab83050d45592548
Kitiyaparnnn <59277374+Kitiyaparnnn@users.noreply.github.com>
2021-02-01 21:15:41 +0700

    my react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import Header, { SubHeader } from "./Header";
+// สามารถเปลี่ยนชื่อ header เป๋็นชื่ออื่นได้
+import { useState, useEffect } from "react";
 
+function Footer() {
+  return <h1>This is footer</h1>;
+}
 function App() {
+  // let counter = 0;
+  // const addCounter = () => {
+  //   counter++;
+  //   console.log(counter);
+  // };
+
+  const [counter, setCounter] = useState(0);
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log("counter change" + counter);
+  }, [counter]);
+
+  const addCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const deleteCounter = () => {
+    if (counter > 0) setCounter(counter - 1);
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Header /> */}
+      {/* {counter} 
+      //react จำ state เดิมไม่เปลี่ยนค่า */}
+      console.log("eiei")
+      <br />
+      {counter}
+      <br />
+      <button onClick={addCounter}>+</button>
+      <button onClick={deleteCounter}>-</button>
+      <button onClick={resetCounter}>reset</button>
+      <p>Parn eiei</p>
+      <Footer />
+    </>
   );
 }
 
